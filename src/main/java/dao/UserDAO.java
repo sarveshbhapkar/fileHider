586becc3e7e89b44abdb9cb2c0c6063cb06f80bc
sarveshbhapkar <sarveshbhapkar100@gmail.com>
2024-05-17 14:53:49 +0530

    java project

---
+++ src/main/java/dao/UserDAO.java
@@ -0,0 +1,36 @@
+package dao;
+
+import db.MyConnection;
+import model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+
+    public static boolean isExist(String email) throws SQLException {
+
+        Connection connection = MyConnection.getConnection();
+        PreparedStatement ps = connection.prepareStatement("select email from users");
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next()){
+            String s = rs.getNString(1);
+            if(email.equals(email))
+                return true;
+
+        }
+        return false;
+
+
+    }
+    public static int saveUser(User user) throws SQLException{
+        Connection connection = MyConnection.getConnection();
+        PreparedStatement ps = connection.prepareStatement("insert into users values(default, ?, ?)");
+        ps.setString(1, user.getName());
+        ps.setString(2, user.getEmail());
+        return ps.executeUpdate();
+    }
+}
---
+++ src/main/java/db/MyConnection.java
@@ -0,0 +1,33 @@
+package db;
+
+import org.example.Main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection = null;
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/fileHider?useSSL=false", "root", "root");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Connection ho gya sir");
+        return connection;
+    }
+    public static void closeConnection() {
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
